3c6a9aeff1c2d214366f966d695d93681bd408ce
Clumsywolfy <debbie.kauw@hotmail.nl>
2021-02-22 12:06:21 +0100

    /

--- src/main/java/nl/han/dea/http/HttpServer.java
+++ src/main/java/nl/han/dea/http/HttpServer.java
@@ -16,14 +16,14 @@ public class HttpServer {
     }
 
     public void startServer() {
-        try (
-                var serverSocket = new ServerSocket(this.tcpPort);
-        ) {
+        try (var serverSocket = new ServerSocket(this.tcpPort);) {
             System.out.println("Server accepting requests on port " + tcpPort);
 
-            var acceptedSocket = serverSocket.accept();
-            var connectionHandler = new ConnectionHandler(acceptedSocket);
-            connectionHandler.handle();
+            while (true) {
+                var acceptedSocket = serverSocket.accept();
+                var connectionHandler = new ConnectionHandler(acceptedSocket);
+                new Thread(() -> connectionHandler.handle()).start();
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
